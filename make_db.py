ec92653abc58bbea91c4eb5c31933d09a145dd2f
Elvis Yung <elvisckyung@gmail.com>
2023-01-08 00:31:58 +0800

    Update make_db.py

--- make_db.py
+++ make_db.py
@@ -6,7 +6,7 @@ import os
 
 filenames_excel = pd.read_csv('file_name.csv')
 
-parent_dir = "/Users/elvisyung/Desktop/t1-research/results_blah/"
+parent_dir = "/Users/elvisyung/Desktop/t1-research/results_folderpurpose/"
 for i in range(len(filenames_excel.index)):
     directory = filenames_excel.file_name[i] + '_file_extension_name'
     path = os.path.join(parent_dir, directory)
